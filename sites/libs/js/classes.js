11837fa2a1885df8f350b2dd56eb676288fb4924
laurentbedu <58790546+laurentbedu@users.noreply.github.com>
2019-12-17 11:53:28 +0100

    Add files via upload

--- sites/libs/js/classes.js
+++ sites/libs/js/classes.js
@@ -14,74 +14,70 @@ class Product{
         this.category_id = category_id;
     }
 
-    faitQqch(){
-        return this;
-    }
-
-    static staticFaitQqch(){
-        return "je suis une fonction static";
-    }
-
+    //Converti un objet json en objet de type Product
     static fromJsonObj(jsonObj){
         return new Product(jsonObj.id, jsonObj.nom, jsonObj.description, jsonObj.prix, jsonObj.category_id)
     }
+    //converti un tableau d'objets json en tableau d'objets Product
     static fromJsonObjArray(jsonObjArray){
         return jsonObjArray.map(elt => Product.fromJsonObj(elt))
     }
 
 }
-
-function convertJsonToProduct(jsonObj){
-
-    return new Product()
+//Création de 6 objets de type (de la classe) Product :
+let prod1 = new Product(1, "Prise 220V", "...", 19.5, 1);
+let prod2 = new Product(2, "Raccord en cuivre diam 50", "...", 5, 2);
+let prod3 = new Product(3, "Bobine de fil bleu, long : 20m", "", 27.8, 1);
+let prod4 = new Product(4, "Bobine de fil rouge, long : 20m", "", 27.8, 1);
+let prod5 = new Product(5, "Robinet style", "...", 77.5, 2);
+let prod6 = new Product(6, "tuyau PER, diam 45, long 2m", "...", 25, 2);
+//Création d'un tableau contenant mes 6 objets Product
+let products = [];
+products.push(prod1, prod2, prod3, prod4, prod5, prod6);
+//Conversion de mon tableau products en string json
+let productsJson = JSON.stringify(products);
+//Envoi d'une requette ajax post vers req.php 
+//pour écrire le fichier json sur le serveur
+let dataToPostProducts = {
+    table: "products",
+    data: productsJson
 }
+$.post("../templates/req.php", dataToPostProducts).done(function(resp){
+    //resp = reponse du serveur (ce que req.php renvoi)
+    let bp;
+});
+//Je vide le tableau products
+products = undefined;
+//Envoi d'une requette ajax post vers get_table.php
+//pour lire le fichier products.json
+$.post("../templates/get_table.php",{table:"products"}).done(function(resp){
+    //resp = reponse du serveur (ce que get_table.php renvoi)
+    products = JSON.parse(resp);//Convertion du texte contenu dans resp en Json
+    products = Product.fromJsonObjArray(products); //Voir classe Products
+    let bp;
+})
 
-let prod = new Product(1, "Prise 220V", "...", 19.5, 1);
-let obj = prod//.faitQqch();
-let val = Product.staticFaitQqch();
-//Classe Category avec :
+//Classe Category
 class Category{
     constructor(id, nom){
         this.id = id;
         this.nom = nom;
     }
+
+
 }
-//Objets :
+//Création de 2 objets de type (de la classe) Category :
 let cat1 = new Category(1, "Electricité");
 let cat2 = new Category(2, "Plomberie");
-let prod1 = new Product(1, "Prise 220V", "...", 19.5, 1);
-//prod1.category_id = 1 donc Electricité
-let prod2 = new Product(2, "Raccord en cuivre diam 50", "...", 5, 2);
-//prod2.category_id = 2 donc Plomberie
-let prod3 = new Product(3, "Bobine de fil bleu, long : 20m", "", 27.8, 1);
-let prod4 = new Product(4, "Bobine de fil rouge, long : 20m", "", 27.8, 1);
-let prod5 = new Product(5, "Robinet style", "...", 77.5, 2);
-let prod6 = new Product(6, "tuyau PER, diam 45, long 2m", "...", 25, 2);
-
-let products = [];
-products.push(prod1, prod2, prod3, prod4, prod5, prod6);
+//Création d'un tableau contenant mes 2 objets Category
 let categories = [];
 categories.push(cat1, cat2);
-let productsJson = JSON.stringify(products);
+//Conversion de mon tableau categories en string json
 let categoriesJson = JSON.stringify(categories);
 
-console.log(productsJson);
-console.log(categoriesJson);
 
-let dataToPost = {
-    table: "products",
-    data: productsJson
-}
-$.post("../templates/req.php", dataToPost).done(function(resp){
-    let bp;
-});
 
-products = undefined;
-$.post("../templates/get_table.php",{table:"products"}).done(function(resp){
-    products = JSON.parse(resp);
-    products = Product.fromJsonObjArray(products); 
-    let bp;
-})
+
 
 
 
