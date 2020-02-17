2a307bc6ea7697ec7713bd0bc88b782e27793d0e
laurentbedu <58790546+laurentbedu@users.noreply.github.com>
2020-02-17 09:15:04 +0100

    Add files via upload

---
+++ phpmvc/index.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <title>MVC</title>
+    <link rel="stylesheet" href="public/bootstrap/bootstrap.min.css">
+    </head>
+<body>
+    <div class="container-fluid">
+    
+    </div>
+</body>
+</html>
